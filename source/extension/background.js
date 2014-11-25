98847109cbe75e9034d6ba606f688ba865c12303
meng <wulalameng@gmail.com>
2014-11-24 20:04:41 -0500

    badge on icon, shortcut, column arrangement

---
+++ source/extension/background.js
@@ -0,0 +1,20 @@
+function updataBadgeText(a){
+	chrome.browserAction.setBadgeText({text: ""+a});
+}
+function reportNumTabs() {
+	chrome.windows.getAll({populate:!0}, function(a) {
+		var b = 0;
+		a.forEach(function(a) {
+			a.tabs&&(b+=a.tabs.length)
+		}
+		);
+		updataBadgeText(b)
+	});
+}
+reportNumTabs();
+chrome.tabs.onCreated.addListener(function (a) {reportNumTabs()});
+chrome.tabs.onRemoved.addListener(function (a) {reportNumTabs()});
+chrome.commands.onCommand.addListener(function(command) {
+	alert('command: ' + command);
+});
+
