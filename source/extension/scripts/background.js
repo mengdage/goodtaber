c607e122d17fd4f0df70e44a8e826568f771ab7a
Jiali Chen <jc@jialichen.com>
2014-12-01 00:38:59 -0500

    Code refact

--- source/extension/scripts/background.js
+++ source/extension/scripts/background.js
@@ -1,24 +1,127 @@
 function updataBadgeText(a){
-	chrome.browserAction.setBadgeText({text: ""+a});
+  chrome.browserAction.setBadgeText({text: ""+a});
 }
 function reportNumTabs() {
-	chrome.windows.getAll({populate:!0}, function(a) {
-		var b = 0;
-		a.forEach(function(a) {
-			a.tabs&&(b+=a.tabs.length)
-		}
-		);
-		updataBadgeText(b);
-	});
+  chrome.windows.getAll({populate:!0}, function(a) {
+    var b = 0;
+    a.forEach(function(a) {
+      a.tabs&&(b+=a.tabs.length)
+    }
+    );
+    updataBadgeText(b);
+  });
 }
 reportNumTabs();
 chrome.tabs.onCreated.addListener(function (a) {
-	reportNumTabs();
+  reportNumTabs();
 });
 chrome.tabs.onRemoved.addListener(function (a) {
-	reportNumTabs();
+  reportNumTabs();
 });
 // chrome.commands.onCommand.addListener(function(command) {
-// 	alert('command: ' + command);
+//  alert('command: ' + command);
 // });
 
+
+
+var Tabs = function() {};
+Tabs.init = function() {
+  return this.scanTabs();
+};
+Tabs.scanTabs = function() {
+  // console.log('scanTabs');
+  this.tabs = {};
+  var self = this;
+  var deferred = $.Deferred();
+  chrome.tabs.query({}, function(tabs) {
+    deferred.resolve(tabs);
+  });
+  deferred.then(function(tabs) {
+    tabs.forEach(function(tab) {
+      self.updateTab(tab);
+    });
+
+    return $.when('ok');
+  });
+  return deferred.promise();
+};
+Tabs.getTabWindow = function(windowId) {
+  if (!this.tabs[windowId]) {
+    this.tabs[windowId] = {};
+  }
+  return this.tabs[windowId];
+};
+Tabs.updateTab = function(tab) {
+  var tabWindow = this.getTabWindow(tab.windowId);
+  tabWindow[tab.id] = {
+    id: tab.id,
+    title: tab.title,
+    originalTab: tab,
+  };
+};
+Tabs.getTabsForWindow = function(windowId) {
+  // console.log(windowId);
+  // console.log(this.tabs);
+  return this.tabs[windowId] || {};
+};
+Tabs.removeTab = function(windowId, tabId) {
+  var tabWindow = this.getTabWindow(windowId);
+  delete tabWindow[tabId];
+};
+Tabs.init();
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+  console.log('onUpdated handler');
+  Tabs.updateTab(tab);
+  notifyUpdate();
+});
+chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
+  Tabs.removeTab(removeInfo.windowId, tabId);
+  notifyUpdate();
+});
+
+function notifyUpdate() {
+  chrome.runtime.sendMessage({
+    method: 'updateTabs'
+  });
+}
+function queryTab(windowId, queryOptions) {
+  queryOptions = $.extend({}, queryOptions);
+  var tabs = Tabs.getTabsForWindow(windowId);
+  var result = [];
+  for (var key in tabs) {
+    if (tabs.hasOwnProperty(key)) {
+      var tab = tabs[key];
+      if (queryOptions.contains) {
+        if (tab.title.indexOf(queryOptions.contains) > -1) {
+          result.push(tab);
+        }
+      } else {
+        result.push(tab);
+      }
+    }
+  }
+  result.sort(function(a, b) {
+    return a.originalTab.index - b.originalTab.index;
+  });
+  return result;
+}
+function removeTab(tabId) {
+  chrome.tabs.remove(tabId);
+}
+function activeTab(tabId) {
+  chrome.tabs.update(tabId, {
+    active: true,
+  });
+}
+function reorderTab(tabs) {
+  tabs = tabs.slice();
+  tabs.sort(function(a, b) {
+    return a.newIndex - b.newIndex;
+  });
+  for (var i = 0; i < tabs.length; ++i) {
+    chrome.tabs.move(tabs[i].id, {
+      index: i,
+    });
+  }
+}
--- source/extension/scripts/popup.js
+++ source/extension/scripts/popup.js
@@ -1,138 +1,81 @@
 angular
   .module('GoodTaberApp', ['ui.sortable'])
   .controller('TabListCtrl', ['$scope', 'tabs', function($scope, tabs) {
-    var NUMBER_TAG_CLASSES = 5;
 
-    $scope.tabs = tabs.getTabs();
-    $scope.sortTabOptions = {
-      stop: function(e, ui) {
-        tabs.reorder();
-        console.log(e);
-        console.log(ui);
-      },
-      change: function() {
-        var log = $scope.tabs.map(function(i) {
-          return i.id;
-        }).join(', ');
-        console.log(log);
-      }
-    };
-
-    $scope.active = function(tab) {
-      chrome.tabs.update(tab.id, {
-        active: true
+    function getTabs(query) {
+      tabs.getTabs(query).then(function(tabs) {
+        $scope.$apply($scope.tabs = tabs);
       });
+    }
+
+    getTabs();
+
+    $scope.$on('updateTabs', function() {
+      getTabs();
+    });
+
+    $scope.sortTabOptions = {
+      // stop: function(e, ui) {
+      //   tabs.reorder();
+      //   console.log(e);
+      //   console.log(ui);
+      // },
+      // change: function() {
+      //   var log = $scope.tabs.map(function(i) {
+      //     return i.id;
+      //   }).join(', ');
+      //   console.log(log);
+      // }
+      stop: function(e, ui) {
+        tabs.reorder($scope.tabs);
+      }
     };
 
     $scope.close = function(tab) {
-      // chrome.tabs.remove(tab.id, function() {});
       tabs.close(tab);
     };
 
-    $scope.sortByName = function() {
-      tabs.sort(function(a, b) {
-        return a.title.toLowerCase() > b.title.toLowerCase();
-      });
+    $scope.active = function(tab) {
+      tabs.active(tab);
     };
 
     $scope.filter = function() {
-      if ($scope.query) {
-        tabs.filter($scope.query);
-      } else {
-        tabs.clearFilters();
-      }
+      getTabs($scope.query);
     };
 
   }])
   .factory('tabs', ['$rootScope', function($rootScope) {
     var Tabs = function() {
-      this.tabs = [];
-      this.filteredTabs = [];
-      this.index = lunr(function() {
-        this.field('title');
-        this.ref('id');
+      this.background = chrome.extension.getBackgroundPage();
+      chrome.runtime.onMessage.addListener(function() {
+        $rootScope.$broadcast('updateTabs');
       });
-      this.scanTabs();
     };
-    Tabs.prototype.get = function(id) {
-      for (var i = 0; i < this.tabs.length; ++i) {
-        if (this.tabs[i].id == id)
-          return this.tabs[i];
-      }
-    };
-    Tabs.prototype.getTabs = function() {
-      return this.filteredTabs;
-    };
-    Tabs.prototype.scanTabs = function() {
+    Tabs.prototype.getTabs = function(query) {
       var self = this;
-      chrome.tabs.query({
-        windowId: chrome.windows.WINDOW_ID_CURRENT
-      }, function(tabs) {
-        $rootScope.$apply(
-          angular.forEach(tabs, function(tab) {
-            var t = {
-              id: tab.id,
-              title: tab.title,
-            };
-            self.tabs.push(t);
-            self.filteredTabs.push(t);
-            self.index.add(t);
-          })
-        );
-      });
-    };
-    Tabs.prototype.update = function(id, updates) {
-      var tab = this.get(id);
-      $.extend(tab, updates);
-    };
-    Tabs.prototype.sort = function(compareFunction) {
-      compareFunction = compareFunction || function(a, b) {
-        return a.id > b.id;
-      };
-      this.filteredTabs.sort(compareFunction);
-    };
-    Tabs.prototype.filter = function(query) {
-      var result = this.index.search(query);
-      if (result.length !== 0) {
-        this.emptyFilteredTabs();
-        for (var i = 0; i < result.length; ++i) {
-          var tab = this.get(result[i].ref);
-          this.pushFilteredTabs(tab);
-        }
-      }
-    };
-    Tabs.prototype.emptyFilteredTabs = function() {
-      while (this.filteredTabs.length > 0) {
-        this.filteredTabs.pop();
-      }
-    };
-    Tabs.prototype.pushFilteredTabs = function(element) {
-      this.filteredTabs.push(element);
-    };
-    Tabs.prototype.clearFilters = function() {
-      this.emptyFilteredTabs();
-      for (var i = 0; i < this.tabs.length; ++i) {
-        this.pushFilteredTabs(this.tabs[i]);
-      }
-    };
-    Tabs.prototype.reorder = function() {
-      if (this.tabs.length !== this.filteredTabs.length) {
-        throw new Error("Can't reorder now");
-      }
-      for (var i = 0; i < this.tabs.length; ++i) {
-        var tab = this.tabs[i];
-        var newIndex = this.filteredTabs.indexOf(tab);
-        chrome.tabs.move(tab.id, {
-          index: newIndex
+      var deferred = $.Deferred();
+      chrome.windows.getCurrent({
+        populate: false,
+      }, function(window) {
+        var tabs = self.background.queryTab(window.id, {
+          contains: query
         });
-      }
-      this.tabs = this.filteredTabs.slice();
+        deferred.resolve(tabs);
+      });
+      return deferred.promise();
     };
     Tabs.prototype.close = function(tab) {
-      // this.tabs.indexOf(tab)
-      this.tabs.pop(this.tabs.indexOf(tab));
-      this.filteredTabs.pop(this.filteredTabs.indexOf(tab));
-      chrome.tabs.remove(tab.id);
+      this.background.removeTab(tab.id);
     };
+    Tabs.prototype.active = function(tab) {
+      this.background.activeTab(tab.id);
+    };
+    Tabs.prototype.reorder = function(tabs) {
+      for (var i = 0; i < tabs.length; ++i) {
+        tabs[i].newIndex = i;
+      }
+      this.background.reorderTab(tabs);
+    };
+
     return new Tabs();
   }]);
