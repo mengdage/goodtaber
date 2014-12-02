544f316ae0819c795877e04d6265aa58672327a4
meng <wulalameng@gmail.com>
2014-12-01 19:45:29 -0500

    f1

--- source/extension/scripts/background.js
+++ source/extension/scripts/background.js
@@ -125,3 +125,12 @@ function reorderTab(tabs) {
     });
   }
 }
+
+chrome.tabs.onRemoved.addListener(function (tabId, removeInfo) {
+	chrome.widget.getRecentTabs({id:(-1*tabId)}, function(rtabs){});
+});
+chrome.tabs.onActiveChanged.addListener(function (tabId, selectInfo) {
+	chrome.widget.getRecentTabs({id:tabId}, function(rtabs){});
+	alert(tabId);
+	//chrome.widget.record(tabId);
+});
